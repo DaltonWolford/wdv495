b6c5f3a60e8e9e4ff05cb9618368f18604533ad1
Dalton Wolford <wolforddalton@yahoo.com>
2020-05-02 16:18:41 -0500

    Add files via upload

---
+++ PythonJSON/outputJSONObject.py
@@ -0,0 +1,36 @@
+#!C:\Users\DaltonLaptop\AppData\Local\Programs\Python\Python38-32\python.exe
+
+print("")
+import json
+
+data = {}
+data['students'] = []
+data['students'].append({
+    'student_major': 'Web Development',
+    'student_id': '332443',
+    'student_gpa': '3.6',
+	'student_courses': ['WDV101','WDV131', 'WDV105']
+})
+data['students'].append({
+    'student_major': 'Web Development',
+    'student_id': '332123',
+    'student_gpa': '3.9',
+	'student_courses': ['WDV321','WDV121', 'WDV131']
+})
+data['students'].append({
+    'student_major': 'Photography',
+    'student_id': '331227',
+    'student_gpa': '3.0',
+	'student_courses': ['CRS205','WDV131', 'SDV108']
+})
+
+with open('students.json', 'w') as outfile:
+    json.dump(data, outfile)
+	
+
+with open('students.json') as json_file:
+    dataSend = json.dumps(data)
+
+
+print((dataSend))
+
